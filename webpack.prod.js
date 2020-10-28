173dc73b3269038da948acd0c8aaf66f4ce89c3a
Brian Glass <bglass@remmac06337.local>
2020-10-28 17:02:20 -0400

    Update webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack')
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
-        new UglifyJSPlugin()
+				new webpack.DefinePlugin({
+					'process.env.PORT': 8000,
+					'process.env.BASE_URL': JSON.stringify('https://orthocal.info')
+        })
     ]
 });
